3e8d582d5c696cb4716d78cf4f46dc195ec49362
Ben Nibbelink <bennibbelink@bens-air.lan>
2023-04-22 16:55:45 -0500

    fixed backend replicas

--- README.md
+++
@@ -1,2 +0,0 @@
-# cs839-backend
-Vyper smart contract for Yahtzee gameplay.
--- migrations/2_deploy_yahtzee.js
+++
@@ -1,6 +0,0 @@
-var Yahtzee = artifacts.require("Yahtzee");
-
-module.exports = async function(deployer) {
-  const accounts = await web3.eth.getAccounts();
-  deployer.deploy(Yahtzee, accounts[1], accounts[2], {from: accounts[1]});
-};
